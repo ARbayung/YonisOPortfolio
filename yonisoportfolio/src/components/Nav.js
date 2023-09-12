57fa67d7e1c486b7426607ae8e9c3e8172818d1b
Abiskar Rai <a.bayung28@gmail.com>
2023-09-12 20:57:46 +0100

    added initial navigation bar with style

---
+++ yonisoportfolio/src/components/Nav.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Nav(){
+    return (
+        <nav>
+            <h1 className="nav-logo">YO</h1>
+                <ul className="nav-links">
+                    <li>ABOUTME</li>
+                    <li>ARCIVES</li>
+                    <li>CONTACT</li>
+                </ul>          
+        </nav>
+    )
+}
+
+export default Nav;
